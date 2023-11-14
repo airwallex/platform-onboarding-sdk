02842ec32d0079c64a219ba08ba4c65fa08e590d
Ruixue Liao <ruixue.liao@airwallex.com>
2023-11-14 18:04:42 +0800

    fix: [OSAI-8108] Deprecate platform-onboarding-sdk

--- src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { getGatewayUrl } from '.';
-
-describe('getGatewayUrl', () => {
-  it('should return correct url', () => {
-    const url = getGatewayUrl('staging');
-    expect(url).toBe('https://static-staging.airwallex.com/widgets/sdk/onboarding/v1/');
-  });
-});
